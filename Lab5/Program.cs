f47ed967536a34a4af8c494c3334b80220350a13
Azarb13 <Zarbavel2006@icloud.com>
2025-08-03 23:46:24 -0400

    Program.cs

--- Lab5/Program.cs
+++ Lab5/Program.cs
@@ -1,9 +1,53 @@
-﻿namespace Lab5;
+namespace Lab5;
 
-class Program
+internal class Program
 {
-    static void Main(string[] args)
+    private static void Main(string[] args)
+    { }
+    // Step 1 Base Class
+    public class Animal
     {
-        Console.WriteLine("Hello, World!");
+        public virtual void animalSound()
+        {
+            Console.WriteLine("The animal makes a sound");
+        }
+    }
+
+    // Step 2 – Derived Class- Cat
+    public class Cat : Animal
+    {
+
+        public override void animalSound()
+        {
+            Console.WriteLine("Cat says: meow");
+        }
+    }
+
+    // Step 2 – Derived Class- Dog
+    public class Dog : Animal
+    {
+
+        public override void animalSound()
+        {
+            Console.WriteLine("Dog says: Wruff, Wruff");
+        }
+    }
+
+    // Step 3 – Test in Main()
+    private class Program
+    {
+        private static void Main(string[] args)
+        {
+            //  objects using Animal as reference type (polymorphism)
+            Animal myAnimal = new Animal(); 
+            Animal myCat = new Cat();       
+            Animal myDog = new Dog();       
+
+            //  animalSound() on each
+            myAnimal.animalSound(); // Should say: The animal makes a sound
+            myCat.animalSound();    // Should say: Cat says: meow
+            myDog.animalSound();    // Should say: Dog says: Wruff
+        }
     }
 }
+
